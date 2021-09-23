fbe15dc56206df06198422c44451515bcdecc5e9
Lasin Ahammed <91196164+lasinkaithavalappil@users.noreply.github.com>
2021-09-23 13:56:15 +0530

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -10,14 +10,14 @@ Asena.addCommand({pattern: 'menu', fromMe: false, desc: Lang.WP}, (async (messag
     var r_text = new Array ();
     
     
-    r_text[0] = "https://www.pixelstalk.net/wp-content/uploads/2016/03/Pikachu-wallpaper-HD-dowload.jpg";
+    r_text[0] = "https://www.linkpicture.com/q/20210922_202155_1.png";
     
     
     var i = Math.floor(1*Math.random())
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `⛦━━━━━✨️Ameer Suhail✨️━━━━━⛦
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `⛦━━━━━✨️Lasin Ahamed✨️━━━━━⛦
 
 
 𝐇𝐞𝐥𝐥𝐨👋 𝐈 𝐚𝐦 𝐚 Lasin Ahamed 𝐛𝐨𝐭.
